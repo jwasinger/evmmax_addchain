8b535f2d65fa37aae92fe99cb937b1f6a0650bf6
Jared Wasinger <j-wasinger@hotmail.com>
2023-02-12 01:09:26 -0500

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-./addchain/cmd/addchain/addchain search '2^255 - 19 - 2' > example.acc
-./addchain/cmd/addchain/addchain gen -tmpl example.go.template example.acc | gofmt > example.go
+./addchain/cmd/addchain/addchain search '0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab' > example.acc
+./addchain/cmd/addchain/addchain gen -tmpl modinv.huff.template example.acc > modinv.huff
