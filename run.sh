3123a265114cdc0518128d3d98efc72df251558a
Jared Wasinger <j-wasinger@hotmail.com>
2023-02-12 01:54:10 -0500

    rename

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 ./addchain/cmd/addchain/addchain search '4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559785' > example.acc
-./addchain/cmd/addchain/addchain gen -tmpl modinv.huff.template example.acc > modinv.huff
+./addchain/cmd/addchain/addchain gen -tmpl modinv.huff.template.template example.acc > modinv.huff.template
