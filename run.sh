2753c9480969c0d29bbd1e6ab7d3c1d19e1aed7d
Jared Wasinger <j-wasinger@hotmail.com>
2023-02-12 01:17:10 -0500

    use correct exponent

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-./addchain/cmd/addchain/addchain search '0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab' > example.acc
+./addchain/cmd/addchain/addchain search '4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559785' > example.acc
 ./addchain/cmd/addchain/addchain gen -tmpl modinv.huff.template example.acc > modinv.huff
